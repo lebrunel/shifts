c607323d82d577e549c44ed9d833bde4d23e2afd
lebrunel <124721263+lebrunel@users.noreply.github.com>
2024-05-16 14:13:51 +0100

    initial commit

---
+++ lib/shifts.ex
@@ -0,0 +1,18 @@
+defmodule Shifts do
+  @moduledoc """
+  Documentation for `Shifts`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Shifts.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/shifts_test.exs
@@ -0,0 +1,8 @@
+defmodule ShiftsTest do
+  use ExUnit.Case
+  doctest Shifts
+
+  test "greets the world" do
+    assert Shifts.hello() == :world
+  end
+end
